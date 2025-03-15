32f096308d53080f9da18e7789f54ed73828fa4d
nengkya <nengkya@yahoo.com>
2025-03-16 05:58:11 +0700

    Field 'id' expected a number but got 'choice.id'.

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'polls'
+
 urlpatterns = [
     #ex: /polls/
-    path('', views.index, name = 'index'),
+    path('', views.index, name = "index"),
 
     #ex: /polls/5
-    path('<int:question_id>/', views.detail, name = 'detail')
+    path('<int:question_id>/', views.detail, name = 'detail'),
+
+    #ex: /polls/5/vote
+    path('<int:question_id>/vote', views.vote, name = 'vote'),
+
+    #ex: /polls/5/result
+    path('<int:question_id>/result', views.results, name = 'results')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,84 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.db.models import F
+from django.urls import reverse
 
 
+'''
 def index(request):
-    #latest_question_list = Question.objects.order_by('pub_date')[:5] old date first
-    #latest_question_list = Question.objects.order_by('-pub_date')[5] error
-    #latest_question_list = Question.objects.order_by('-pub_date')[5:] 5th index
+    latest_question_list = Question.objects.order_by('pub_date')[:5] old date first
+    latest_question_list = Question.objects.order_by('-pub_date')[5] error
+    latest_question_list = Question.objects.order_by('-pub_date')[5:] 5th index
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list' : latest_question_list}
+    return HttpResponse(template.render(context, request))
+'''
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list' : latest_question_list}
+    return render(request, 'polls/index.html', context) 
+
+
+'''
+def detail(request, question_id):
+    return HttpResponse('You are looking at %s' %  question_id)
+
 
 def detail(request, question_id):
-    return HttpResponse('You are looking at %s', question_id)
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        #without raise Http404 will occur UnboundLocalError at /polls/111/
+        raise Http404('Question HaGa does not exist !')
+
+    return render(request, 'polls/detail.html', {'question' : question})
+'''
+
+
+def detail(request, question_id):
+    
+    question = get_object_or_404(Question, pk = question_id)
+
+    #'question' is for detail.html {{ question }}
+    return render(request, 'polls/detail.html', {'question' : question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Question.DoesNotExist): #KeyError = pk
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                "question" : question,
+                'error_message' : 'You did not select any choice !'
+            }
+        )
+    else:
+        selected_choice.votes = F('vote') + 1 
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args = (question.id, )))
+
+
+def results(request, question_id):
+    response = 'You are looking at the result of question %s'
+    return HttpResponse(response % question_id)
+
+
+
+
+
+
+
