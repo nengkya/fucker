4572539f788946b5b4b2246fc872517dfa812855
nengkya <nengkya@yahoo.com>
2025-03-17 00:23:10 +0700

    fix <input type=radio name=choice id=choice{{forloop.counter}} value=choice.id>

--- polls/views.py
+++ polls/views.py
@@ -67,18 +67,11 @@ def vote(request, question_id):
             }
         )
     else:
-        selected_choice.votes = F('vote') + 1 
+        selected_choice.votes = F('votes') + 1 
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:result', args = (question.id, )))
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id, )))
 
 
 def results(request, question_id):
-    response = 'You are looking at the result of question %s'
-    return HttpResponse(response % question_id)
-
-
-
-
-
-
-
+    question = get_object_or_404(Question, pk = question_id);
+    return render(request, 'polls/results.html', {'question' : question})
