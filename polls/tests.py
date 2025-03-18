6ec43d7afbf526b99c805891a94f11f1f1ee94ac
nengkya <nengkya@yahoo.com>
2025-03-19 01:03:20 +0700

    self.assertQuerySetEqual(response.context[latest_question_list], [question])

--- polls/tests.py
+++ polls/tests.py
@@ -46,15 +46,22 @@ class QuestionIndexViesTests(TestCase):
         #if no questions exist, an appropriate message is displayed
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'No polls are available.')
+        self.assertContains(response, 'No HaGa polls are available !')
+        self.assertQuerySetEqual(response.context['latest_question_list'], [])
 
+    def test_past_question(self):
+        #question with a pub_date in the past are displayed on the index page
+        question = create_question(question_text = 'Past question.', days = -30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerySetEqual(response.context['latest_question_list'], [question])
 
+    def test_future_question(self):
+        #question with a pub_date in the future are not displayed on the index page
+        question = create_question(question_text = 'Future question.', days = 30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerySetEqual(response.context['latest_question_list'], [])
 
-
-
-
-
-
+      
 
 
 
